fe87cfc9c3f18c64264f4132aeafc8bd158a6824
Wayne Graham <wgraham@clir.org>
2017-11-30 13:58:36 -0500

    Create custom image sizes
    Creates cutom image sizes for use in WordPress. This may be unneccessary as the JetPack photon plugin will do this dynamically
    
    closes #2

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
-
 function enqueue_parent_styles()
 {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -19,3 +18,9 @@ function enqueue_parent_styles()
   // custom scripts - be sure to compile this with Gulp task (e.g. gulp js)
   wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'));
 }
+
+add_theme_support('post-thumbnails');
+
+add_image_size('background-image', 1920, 1000, true);
+add_image_size('background-tablet', 1024, 540);
+add_image_size('background-mobile', 768, 768);
